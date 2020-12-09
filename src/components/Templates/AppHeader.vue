eb41b0400da04e581c1b7006f1c2decd6a782f59
Leonardo Adamoli <leonardo.adamoli@possible.com>
2020-12-09 09:17:44 -0300

    [WIP] Turning Header responsible

--- src/components/Templates/AppHeader.vue
+++ src/components/Templates/AppHeader.vue
@@ -1,24 +1,59 @@
 <template>
-  <v-app-bar app hide-on-scroll dark>
-    <v-toolbar-title>
-      Vue MeetUp
-    </v-toolbar-title>
+  <div class="header-wrapper">
+    <v-navigation-drawer
+      v-model="sideNav"
+      absolute
+      left
+      temporary
+    >
+      <v-list>
+        <v-list-item>
+          <v-list-item-action>
+            <v-icon>
+              calendar_today
+            </v-icon>
+          </v-list-item-action>
 
-    <v-spacer />
+          <v-list-item-content>
+            Events
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-    <v-toolbar-items>
-      <v-btn text>
-        <v-icon left>
-          calendar_today
-        </v-icon>
-        Eventos
-      </v-btn>
-    </v-toolbar-items>
-  </v-app-bar>
+    <v-app-bar app hide-on-scroll dark>
+      <v-app-bar-nav-icon
+        @click.native.stop="
+          sideNav = !sideNav
+        "
+        class="d-flex d-sm-none"
+      />
+
+      <v-toolbar-title>
+        Vue MeetUp
+      </v-toolbar-title>
+
+      <v-spacer />
+
+      <v-toolbar-items
+        class="d-none d-sm-flex"
+      >
+        <v-btn text>
+          <v-icon left>
+            calendar_today
+          </v-icon>
+          Events
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'AppHeader'
+  name: 'AppHeader',
+  data: () => ({
+    sideNav: false
+  })
 }
 </script>
